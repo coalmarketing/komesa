74169a241ebd7542be64c4f83b6e968a1cc53060
Tomas Chotenovsky <tomas.chotenovsky@coalsoft.cz>
2026-04-27 13:46:44 +0200

    ztrácím to

--- app/api/references/route.ts
+++ app/api/references/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import { createPool } from '@/lib/db';
-const { env } = require('node:process');
 
 
 interface Reference {
@@ -63,9 +62,6 @@ const getCorsHeaders = () => {
 
 export async function GET() {
   console.log('Public: Začátek načítání referencí');
-  console.error(process);
-  console.error("env", process.env);
-  console.log("node env", env);
   try {
     // Kontrola, zda je k dispozici DATABASE_URL
     console.log('Kontrola proměnných prostředí:', {
